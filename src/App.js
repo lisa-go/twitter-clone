16eae1c51f925a0d69710659ca64d40c59fa3388
Lisa <lis-zaa@live.com>
2022-12-14 03:48:21 -0600

    able to retrieve data from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Home from "./components/Home";
 
 export default function App() {
   return (
     <div> 
-      Hello 
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function Home() {
+    const [loading, setLoading] = useState(false);
+
+    const [data, setData] = useState([{}]);
+
+    useEffect(() => {
+        setLoading(true);
+        axios({
+            method: 'GET',
+            url: 'https://animechan.vercel.app/api/quotes/anime?title=Hunter%20x%20hunter'
+        })
+            .then(Response => {
+                setData(Response.data);
+                console.log(Response.data);
+            })
+            .catch(Error => { console.log(Error) })
+            .finally(() => { setLoading(false) })
+    }, []);
+
+    return (
+
+        <div id="tweetsContainer">
+            {loading ?
+                <div>loading...</div>
+                :
+                data.map((dt) =>
+                    <div className="tweet">
+                        <div>{dt.character}</div>
+                        <div>{dt.quote}</div>
+                    </div>
+                )}
+        </div>
+
+    )
+}
