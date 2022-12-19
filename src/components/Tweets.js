dc8f563c4344b36e514cda70e50048f0f6c2e0c7
Lisa <lis-zaa@live.com>
2022-12-18 22:15:45 -0600

    insert my own db

--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -6,7 +6,7 @@ export default function Tweets ({ loading, setLoading, data, setData }) {
         setLoading(true);
         axios({
             method: 'GET',
-            url: 'https://animechan.vercel.app/api/quotes/anime?title=Hunter%20x%20hunter'
+            url: 'http://localhost:8080/home'
         })
             .then(Response => {
                 setData(Response.data);
@@ -14,7 +14,10 @@ export default function Tweets ({ loading, setLoading, data, setData }) {
             })
             .catch(Error => { console.log(Error) })
             .finally(() => { setLoading(false) })
+           
     }, []);
+
+
 
     return (
 
@@ -23,22 +26,24 @@ export default function Tweets ({ loading, setLoading, data, setData }) {
                 <div>loading...</div>
                 :
                 data.map((dt) =>
-                    <div className="tweet">
-                        <div className="picture">img</div>
+                    <div className="tweet" key={dt._id}>
+                        <div className="picture">
+                            <img src={dt.picon} alt={dt.pname} />
+                        </div>
 
                         <div className="text">
                             <div className="twtHead">
-                                <div className="twtName">{dt.character}</div>
-                                <div>@{dt.character}</div>
-                                <div>• time</div>
+                                <div className="twtName">{dt.pname}</div>
+                                <div>{dt.pusername}</div>
+                                <div>• {dt.date}</div>
                             </div>
                             
-                            <div className="twtContent">{dt.quote}</div>
+                            <div className="twtContent">{dt.twtcontent}</div>
 
                             <div className="twtBadges">
-                                <span>badge1</span>
-                                <span>badge2</span>
-                                <span>badge3</span>
+                                <span>{dt.replies}</span>
+                                <span>{dt.retweets}</span>
+                                <span>{dt.likes}</span>
                                 <span>badge4</span>
                             </div>
                         </div>
