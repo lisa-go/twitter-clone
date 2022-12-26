bcfbde4a621faafd52d9382db296b4794c9b55aa
Lisa <lis-zaa@live.com>
2022-12-25 19:31:53 -0600

    change up random icons in case of image error

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -1,10 +1,24 @@
 import { FiSearch } from 'react-icons/fi';
+import cat0 from "../images/cat(0).png";
+import cat1 from "../images/cat(1).png";
+import cat2 from "../images/cat(2).png";
+import cat3 from "../images/cat(3).png";
+import cat4 from "../images/cat(4).png";
+import cat5 from "../images/cat(5).png";
+import cat6 from "../images/cat(6).png";
+import cat7 from "../images/cat(7).png";
+import cat8 from "../images/cat(8).png";
+import cat9 from "../images/cat(9).png";
+
 var moment = require('moment');
 
 export default function Discover({ top }) {
 
+    const defaultImages = [cat0, cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9]
+
     const defaultIcon = (e) => {
-        e.target.src = 'https://i.imgur.com/b8fNcS2.png';
+        let pic = Math.floor(Math.random() * 10);
+        e.target.src = defaultImages[pic];
     }
 
     return (
